4861f3d3a1ac5b48296a2b38951514b0dc718e99
Daniel <58704522+danielpms@users.noreply.github.com>
2019-12-20 20:12:44 +0100

    New main interface and new help activity with webview

--- app/src/main/java/com/sds/puzzledroid/MainActivity.java
+++ app/src/main/java/com/sds/puzzledroid/MainActivity.java
@@ -2,9 +2,13 @@ package com.sds.puzzledroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
 
     //Shows Action Bar Menu
@@ -26,13 +33,19 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch(id) {
             case R.id.config_item:
-                Toast.makeText(this, "Configuración", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "En construcción ...", Toast.LENGTH_SHORT).show();
                 return true;
 
             case R.id.help_item:
-                Toast.makeText(this, "Ayuda", Toast.LENGTH_SHORT).show();
+                onClickGoTo(this.findViewById(R.id.help_item));
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Go to another Activity
+    public void onClickGoTo(View view) {
+        Intent i = new Intent(this, HelpActivity.class);
+        startActivity(i);
     }
 }
