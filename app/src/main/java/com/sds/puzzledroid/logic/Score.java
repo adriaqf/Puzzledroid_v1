eb68d2632f71c19731def360f7ae8438528703f0
Daniel <58704522+danielpms@users.noreply.github.com>
2020-01-08 11:21:13 +0100

    SQLiteScore and Score classes implemented

--- app/src/main/java/com/sds/puzzledroid/logic/Score.java
+++ app/src/main/java/com/sds/puzzledroid/logic/Score.java
@@ -1,4 +1,41 @@
 package com.sds.puzzledroid.logic;
 
 public class Score {
+
+    private int totalScore;
+    private int difficulty;
+    private String dateTime;
+
+    public Score() {}
+
+    public Score(int totalScore, int difficulty, String dateTime) {
+        this.totalScore = totalScore;
+        this.difficulty = difficulty;
+        this.dateTime = dateTime;
+    }
+
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
 }
--- app/src/main/java/com/sds/puzzledroid/sqlite/SQLiteScore.java
+++ app/src/main/java/com/sds/puzzledroid/sqlite/SQLiteScore.java
@@ -1,4 +1,59 @@
 package com.sds.puzzledroid.sqlite;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+
+import com.sds.puzzledroid.logic.Score;
+
+import java.util.ArrayList;
+
 public class SQLiteScore {
+
+    private Context context;
+    private Score score;
+    private AdminOpenHelper admin;
+
+    public SQLiteScore(Context context, Score score) {
+        this.context = context;
+        this.score = score;
+    }
+
+    public boolean insertScore(Score score) {
+        admin = new AdminOpenHelper(context);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("difficulty", score.getDifficulty());
+        contentValues.put("timeSecs", score.getTotalScore());
+        contentValues.put("dateScore", score.getDateTime());
+
+        db.insert("score", null, contentValues);
+
+        return true;
+    }
+
+    public ArrayList<Score> getAllScores() {
+        ArrayList<Score> scoreArrayList = new ArrayList<>();
+        Score score = new Score();
+
+        admin = new AdminOpenHelper(context);
+        SQLiteDatabase db = admin.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from score", null);
+        cursor.moveToFirst();
+
+        while(!cursor.isAfterLast()) {
+            score.setDifficulty(cursor.getInt(cursor.getColumnIndex("difficulty")));
+            score.setTotalScore(cursor.getInt(cursor.getColumnIndex("timeSecs")));
+            score.setDateTime(cursor.getString(cursor.getColumnIndex("dateScore")));
+            scoreArrayList.add(score);
+
+            cursor.moveToNext();
+        }
+
+        return scoreArrayList;
+    }
+
 }
