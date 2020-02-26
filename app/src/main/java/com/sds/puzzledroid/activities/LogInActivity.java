0e40728f17f4ebee520a6079ee8f2d38947ec5cd
adriaqf <51478878+adriaqf@users.noreply.github.com>
2020-02-26 13:38:57 +0100

    Location fixed V2

--- app/src/main/java/com/sds/puzzledroid/activities/LogInActivity.java
+++ app/src/main/java/com/sds/puzzledroid/activities/LogInActivity.java
@@ -2,6 +2,7 @@ package com.sds.puzzledroid.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -10,9 +11,12 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -54,81 +58,101 @@ public class LogInActivity extends AppCompatActivity {
 
         tvGPS = findViewById(R.id.tvGPS);
 
-        Permissions permissions = new Permissions(this);
-        permissions.verifyPermissions();
-
-       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1000);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
         } else {
-        LocationManager locationManager = (LocationManager) getSystemService((Context.LOCATION_SERVICE));
-        Location location = locationManager.getLastKnownLocation((LocationManager.GPS_PROVIDER));
-        double longitude = location.getLongitude();
-        double latitude = location.getLatitude();
+            locationStart();
+        }
+    }
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+      //  mensaje1.setText("Localización agregada");
+       // mensaje2.setText("");
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
             try {
-                Geocoder gcd = new Geocoder(this, Locale.getDefault());
-                List<Address> addresses = gcd.getFromLocation(latitude, longitude, 1);
-                if (addresses.size() > 0) {
-                    System.out.println(addresses.get(0).getLocality());
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                  //  tvGPS.setText("Mi direccion es: \n"  + DirCalle.getAddressLine(0));
+                    tvGPS.setText(list.get(0).getCountryCode()+" " + list.get(0).getLocality()+"\n"+list.get(0).getAddressLine(0));
                 }
-              tvGPS.setText(addresses.get(0).getCountryName());
-            } catch (Exception e){
-                System.out.println(e.getMessage());
-                Toast.makeText(this, "Not 55found!", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        }
-        //String all = getResources().getConfiguration().locale.getCountry();
-        //tvGPS.setText(all);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        switch (requestCode){
-            case 1000:
-                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                    LocationManager locationManager = (LocationManager) getSystemService((Context.LOCATION_SERVICE));
-                    Location location = locationManager.getLastKnownLocation((LocationManager.GPS_PROVIDER));
-                    try {
-                        double longitude = location.getLongitude();
-                        double latitude = location.getLatitude();
-                        Geocoder gcd = new Geocoder(this, Locale.getDefault());
-                        List<Address> addresses = gcd.getFromLocation(latitude, longitude, 1);
-                        if (addresses.size() > 0) {
-                            System.out.println(addresses.get(0).getLocality());
-                        }
-                        tvGPS.setText(addresses.get(0).getCountryName());
-                    }catch (Exception e){
-                        e.printStackTrace();
-                        Toast.makeText(this, "Not found!", Toast.LENGTH_SHORT).show();
-                    } }else{
-                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
-                }
-                break;
         }
     }
-
-    private String hereLocation(double lat, double lon) {
-        String cityName = "";
-        String algo = "";
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        List<Address> adresses;
-        try {
-            adresses = geocoder.getFromLocation(lat, lon, 10);
-            if(adresses.size()>0){
-                for (Address adr: adresses){
-                    if(adr.getLocality()!= null && adr.getLocality().length()>0){
-                        cityName = adr.getLocality();
-                        algo = adr.getCountryName();
-                        break;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    /* Aqui empieza la Clase Localizacion */
+    public class Localizacion implements LocationListener {
+        LogInActivity mainActivity;
+        public LogInActivity getMainActivity() {
+            return mainActivity;
+        }
+        public void setMainActivity(LogInActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+            loc.getLatitude();
+            loc.getLongitude();
+            String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+            //mensaje1.setText(Text);
+            this.mainActivity.setLocation(loc);
 
         }
-        return cityName;
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es desactivado
+
+            Toast.makeText(mainActivity, " GPS Desactivado", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es activado
+
+            Toast.makeText(mainActivity, " GPS Activado", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
     }
 
 
